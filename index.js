9902e6ad6fe33d78f93c5751de837e06fb0a5c86
hasanfuad <hasanmdfuad@hotmail.com>
2021-09-07 22:07:06 +0600

    final commit mongodb crud

--- index.js
+++ index.js
@@ -1,20 +1,9 @@
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const bodyParser = require("body-parser");
-const ObjectId = require("mongodb").ObjectId;
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectId;
 
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-app.use = bodyParser.json();
-
-// const dbUser = "organicProduct";
-// const dbPass = "2zG20hqySsaxatsa";
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+// const password = '2zG20hqySsaxatsa';
 
 const uri =
   "mongodb+srv://organicProduct:2zG20hqySsaxatsa@cluster0.shttn.mongodb.net/organicDb?retryWrites=true&w=majority";
@@ -24,61 +13,57 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const productCollection = client.db("organicDb").collection("products");
-
-app.get("/product/:id", (req, res) => {
-  productCollection.find({_id: ObjectId(req.params.id)})
-  .toArray((err, documents)=>{
-    res.send(documents[0]);
-  })
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
 })
 
+client.connect(err => {
+  const productCollection = client.db("organicDb").collection("products");
+  
+  app.get('/products', (req, res) => {
+    productCollection.find({})
+    .toArray( (err, documents) =>{
+      res.send(documents);
+    })
+  })
 
-
-client.connect((err) => {
+  app.get('/product/:id', (req, res) => {
+    productCollection.find({_id: ObjectId(req.params.id)})
+    .toArray ( (err, documents) =>{
+      res.send(documents[0]);
+    })
+  })
+  
   app.post("/addProduct", (req, res) => {
     const product = req.body;
     productCollection.insertOne(product)
     .then(result => {
-        console.log("data added successfully");
-        res.redirect("/");
+      res.redirect('/')
     })
+  })
 
-  });
+  app.patch('/update/:id', (req, res) => {
+    productCollection.updateOne({_id: ObjectId(req.params.id)},
+    {
+      $set: {price: req.body.price, quantity: req.body.quantity}
+    })
+    .then (result => {
+      res.send(result.modifiedCount > 0)
+    })
+  })
+
+  app.delete('/delete/:id', (req, res) =>{
+    productCollection.deleteOne({_id: ObjectId(req.params.id)})
+    .then( result => {
+      res.send(result.deletedCount > 0);
+    })
+  })
+
 });
 
 
-client.connect(err => {
-
-    app.get("/products", (req, res) => {
-        productCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-
-        })
-    })
-})
-
-app.patch("/update/:id", (req, res) => {
-  productCollection.updateOne({_id: ObjectId(req.params.id)},
-  {
-    $set: {price: req.body.price, quantity: req.body.quantity}
-  })
-  .then(result => {
-    console.log(result);
-  })
-  
-})
-
-app.delete("/delete/:id", (req, res) => {
-  productCollection.deleteOne({_id: ObjectId(req.params.id)})
-  .then( result => {
-    res.send(result.deletedCount > 0)
-  })
-})
-
-
-app.listen("3000", () => {
-  console.log("Server is running on 3000");
-});
+app.listen(3000);
